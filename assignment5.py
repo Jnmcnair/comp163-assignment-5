e524f5e193fd2d68b2a32201c8b3162043cfa79c
Jnmcnair <tchalla12666@gmail.com>
2025-10-03 16:38:05 -0400

    comments

--- assignment5.py
+++ assignment5.py
@@ -1,8 +1,8 @@
 print("=== Challenge 1: Collatz Conjecture ===")
 n = int(input("Enter starting number: "))
 counter = 0
-print("Sequence:", n, end=" ")
-while n != 1:
+print("Sequence:", n, end=" ") #print the starting number and formatting for the rest of the sequence
+while n != 1: #loop until n is 1
     if (n % 2) == 0:
         n/=2
         counter +=1
@@ -18,11 +18,11 @@ print("")
 print("=== Challenge 2: Prime Number Checker ===")
 num=int(input("Enter a number: "))
 print(f"Testing divisors from 2 to {num-1}...")
-if num > 1:
+if num > 1: #check if number is greater than 1 in order to then check if the number is prime
     for i in range(2,num-1):
         if num % i == 0:
             print(num, "is not prime (Divisible by", i,")")
-            break
+            break#add a break statement to exit the loop if a divisor is found since it would not be prime
     else:
         print(num, "is prime!")
 
@@ -33,11 +33,11 @@ print("")
 print("=== Challenge 3: Multiplication Table ===")
 print("Multiplication Table:")
 print("   ", end="")
-for i in range(1,11):
+for i in range(1,11): #print the header row of 1 through 10 
     print(f"{i:4}", end="")
 print()
 
-for row in range(1,11):
+for row in range(1,11): #print each row of the multiplication table 
     print(f"{row:4}", end=" ")
     for col in range(1,11):
         print(f"{row*col:4}", end=" ")
