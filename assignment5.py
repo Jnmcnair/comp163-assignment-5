961176025da2209a5addc23fe817daabb63268e5
Jnmcnair <tchalla12666@gmail.com>
2025-10-02 21:25:04 -0400

    Challenge 3: Multiplication grid - nested loops for 2D data

--- assignment5.py
+++ assignment5.py
@@ -12,15 +12,33 @@ while n != 1:
         counter +=1
         print(int(n), end=" ")
 print()
-print("\nSteps: ", counter)
+print("Steps:", counter)
+print("")
+
 print("=== Challenge 2: Prime Number Checker ===")
 num=int(input("Enter a number: "))
 print(f"Testing divisors from 2 to {num-1}...")
-
-for i in range(2,num):
-    if (num % i) == 0:
-        print(num, "is not prime (Divisible by", i,")")
-        break
+if num > 1:
+    for i in range(2,num-1):
+        if num % i == 0:
+            print(num, "is not prime (Divisible by", i,")")
+            break
     else:
         print(num, "is prime!")
-        break
+
+else:
+    print(num, "is not prime (Less than 2)")
+
+print("")
+print("=== Challenge 3: Multiplication Table ===")
+print("Multiplication Table:")
+print("   ", end="")
+for i in range(1,11):
+    print(f"{i:4}", end="")
+print()
+
+for row in range(1,11):
+    print(f"{row:4}", end=" ")
+    for col in range(1,11):
+        print(f"{row*col:4}", end=" ")
+    print()
